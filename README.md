349ae09e533a794a01b10e7c609069a16da63c58
Randall Reynolds <randall.weylin.reynolds@gmail.com>
2019-03-06 21:40:40 -0500

    Initialized packaging and readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# rwrpy
+
+rwrpy
+Written by Randall Reynolds
+randall.weylin.reynolds@gmail.com
+
+A python library for:
+
+1) Signal Processing
+2) UI Development
+3) Engineering Math
+4) Physics Math
+5) Communication
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='rwrpy',
+	version='0.1',
+	description='RWR Python library. Contains modules for engineering physics, data collection and user interaces',
+	url='https://github.com/rwreynol/rwrpy',
+	author='Randall Reynolds',
+	author_email='randall.weylin.reynolds@gmail.com',
+	license='None',
+	packages=[],
+	install_requires=['numpy'],
+	zip_safe=False)
